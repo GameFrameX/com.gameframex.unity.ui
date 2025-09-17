577a1ffa821157ba22cfe3d3de3cae957136b84a
Blank <wangfj11@foxmail.com>
2025-09-17 11:03:11 +0800

    [增加]  界面组名称的常量定义

---
+++ Runtime/UIGroupNameConstants.cs
@@ -0,0 +1,104 @@
+﻿// GameFrameX 组织下的以及组织衍生的项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
+// 
+// 本项目主要遵循 MIT 许可证和 Apache 许可证（版本 2.0）进行分发和使用。许可证位于源代码树根目录中的 LICENSE 文件。
+// 
+// 不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动！任何基于本项目二次开发而产生的一切法律纠纷和责任，我们不承担任何责任！
+
+namespace GameFrameX.UI.Runtime
+{
+    /// <summary>
+    /// 界面组名称常量
+    /// </summary>
+    public static class UIGroupNameConstants
+    {
+        /// <summary>
+        /// 隐藏
+        /// </summary>
+        public const string Hidden = "Hidden";
+
+        /// <summary>
+        /// 背景
+        /// </summary>
+        public const string Background = "Background";
+
+        /// <summary>
+        /// 场景
+        /// </summary>
+        public const string Scene = "Scene";
+
+        /// <summary>
+        /// 世界
+        /// </summary>
+        public const string World = "World";
+
+        /// <summary>
+        /// 战斗
+        /// </summary>
+        public const string Battle = "Battle";
+
+        /// <summary>
+        /// 头顶
+        /// </summary>
+        public const string Hud = "Hud";
+
+        /// <summary>
+        /// 地图
+        /// </summary>
+        public const string Map = "Map";
+
+        /// <summary>
+        /// 底板
+        /// </summary>
+        public const string Floor = "Floor";
+
+        /// <summary>
+        /// 正常
+        /// </summary>
+        public const string Normal = "Normal";
+
+        /// <summary>
+        /// 固定
+        /// </summary>
+        public const string Fixed = "Fixed";
+
+        /// <summary>
+        /// 窗口
+        /// </summary>
+        public const string Window = "Window";
+
+        /// <summary>
+        /// 提示
+        /// </summary>
+        public const string Tip = "Tip";
+
+        /// <summary>
+        /// 引导
+        /// </summary>
+        public const string Guide = "Guide";
+
+        /// <summary>
+        /// 黑板
+        /// </summary>
+        public const string BlackBoard = "BlackBoard";
+
+        /// <summary>
+        /// 对话
+        /// </summary>
+        public const string Dialogue = "Dialogue";
+
+        /// <summary>
+        /// Loading 
+        /// </summary>
+        public const string Loading = "Loading";
+
+        /// <summary>
+        /// 通知
+        /// </summary>
+        public const string Notify = "Notify";
+
+        /// <summary>
+        /// 系统顶级
+        /// </summary>
+        public const string System = "System";
+    }
+}
